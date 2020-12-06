504c4dad19b65ab48da08a800fa418017adc4f0a
ZackPoly <zpolytse@gmail.com>
2020-12-06 21:45:45 +0200

    added bow

---
+++ bag_of_words.h
@@ -0,0 +1,51 @@
+#define BOW_B_NUM 1001
+#define BOW_STEP 100
+
+// typedef struct Hash_For_Id_Entry* Hashed_Id ;
+// struct Hash_For_Id_Entry ;
+//
+// typedef struct Ids_Bucket* Hash_For_Id;
+// struct Ids_Bucket ;
+//
+// typedef struct Hash_For_Site_Entry* Hashed_Site ;
+// struct Hash_For_Site_Entry ;
+//
+typedef struct Sites_Bucket* Hash_For_Site;
+struct Sites_Bucket ;
+//
+// typedef struct str_node* str_list ;
+// struct str_node ;
+//
+// typedef struct Spec_node* specs ;
+// struct Spec_node ;
+
+
+typedef struct Dictionary_Word* Hashed_Word ;
+struct Dictionary_Word{
+  char* word ;
+  int index ;
+
+  Hashed_Word next ;
+};
+
+// struct for bucket of hash table for dictioary words.
+typedef struct Words_Bucket* Hash_For_Word;
+struct Words_Bucket{
+  Hashed_Word root ;
+};
+
+typedef struct Bag_Of_Words* BoW ;
+struct Bag_Of_Words{
+  Hash_For_Word dict ;
+  int dict_len ;
+
+  float** values ;
+  int bow_len ;
+};
+
+Hashed_Word search_word_in_hash(Hash_For_Word,char*,int) ;
+
+
+Hashed_Word insert_word_in_hash(BoW,char*,int) ;
+
+BoW initialize_bow(Hash_For_Site,BoW,int,int,int) ;
--- id_hash.h
+++ id_hash.h
@@ -1,12 +1,13 @@
 #include "hash.h"
 
-int hash_func(char*,int) ;
-
 Hashed_Id search_id_in_hash(Hash_For_Id,char*,int) ;
 
                                     //  id   file-data
 Hashed_Id insert_id_in_hash(Hash_For_Id,int,char*,FILE*) ;
 
+void append_negative(Hashed_Id, Hashed_Id) ;
+
+void delete_negatives(comp_head) ;
 
 void delete_complex_node(Hashed_Id) ;
 
